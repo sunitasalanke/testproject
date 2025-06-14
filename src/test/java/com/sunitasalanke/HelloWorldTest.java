0ae7c7d39c2b398594a56658b06724a2ca1365d9
sunita <sunitasalanke@gmail.com>
2025-06-14 03:17:50 -0700

    Added HelloWorldTest.java unit test

---
+++ src/test/java/com/sunitasalanke/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.sunitasalanke;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testMain() {
+        assertTrue(true); // dummy test
+    }
+}
