b74c6476f60040a8fc3e13430abcd6f60bc3b046
sunita <sunitasalanke@gmail.com>
2025-07-27 11:52:33 -0700

    Added commands to create directories and files

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-provider "null" {}
-
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo Hello, Terraform!"
+    command = <<EOT
+      mkdir -p /home/sunita/testdir1
+      mkdir -p /home/sunita/testdir2
+      touch /home/sunita/testdir1/file1.txt
+      touch /home/sunita/testdir2/file2.txt
+    EOT
   }
 }
+
+
+
+
+
+
+
